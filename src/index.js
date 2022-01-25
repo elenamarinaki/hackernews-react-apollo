a44342f2f825fa87ff9407195ca9772b6545e9c6
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-01-25 01:12:33 +0000

    commenting the steps

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 import "./styles/index.css"
 import App from "./components/App"
 
+// dependencies to wire up the apollo client
 import {
   ApolloProvider,
   ApolloClient,
@@ -10,10 +11,16 @@ import {
   InMemoryCache,
 } from "@apollo/client"
 
+/**
+ * this will connect our ApolloClient with the GraphQL API
+ * the GraphQL server will be running on http://localhost:4000
+ */
 const httpLink = createHttpLink({ uri: "http://localhost:4000" })
 
+// instantiating apollo
 const client = new ApolloClient({ link: httpLink, cache: new InMemoryCache() })
 
+// the App is wrapped with the higher-order component
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
