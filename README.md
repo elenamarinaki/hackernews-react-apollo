eedbbba2d6fce86b1a2f3f7b16a8bd1c629bb95f
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-02-04 22:58:24 +0000

    basic description of the backend (server)

--- README.md
+++ README.md
@@ -15,3 +15,16 @@
 #### `graphql`
 
 - Facebook's implementation of `GraphQL`
+
+## Server
+
+### Prisma
+
+- `schema.prisma` -> our data model for the project. Uses PSL (Prisma Schema Language) to define the **shape of our database** tables and the **relationships among them**
+- `dev.db` -> SQLite database that will be used here
+
+### Src
+
+- `schema.graphql` -> **Application schema**. It defines the GraphQL operations we can send from the frontend
+- `resolvers` -> contains the resolver functions for the operations that we define in the application schema
+- `index.js` -> entry point for the `GraphQL` server
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom"
 import "./styles/index.css"
 import App from "./components/App"
 
-// dependencies to wire up the apollo client
+// 1️⃣ dependencies to wire up the apollo client
 import {
   ApolloProvider,
   ApolloClient,
@@ -11,16 +11,16 @@ import {
   InMemoryCache,
 } from "@apollo/client"
 
-/**
+/** 2️⃣
  * this will connect our ApolloClient with the GraphQL API
  * the GraphQL server will be running on http://localhost:4000
  */
 const httpLink = createHttpLink({ uri: "http://localhost:4000" })
 
-// instantiating apollo
+// 3️⃣ instantiating apollo
 const client = new ApolloClient({ link: httpLink, cache: new InMemoryCache() })
 
-// the App is wrapped with the higher-order component
+// 4️⃣ the App is wrapped with the higher-order component
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
