5eee308059b3f33b44b08fafebe9f2d5158bff5e
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-02-11 23:55:00 +0000

    login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 export const Login = () => {
@@ -9,5 +9,37 @@ export const Login = () => {
     password: "",
     name: "",
   })
-  return <div>Login</div>
+  return (
+    <div>
+      <h4 className='mv3'>{formState.login ? "Login" : "Sign Up"}</h4>
+      <div className='flex flex-column'>
+        {!formState.login && (
+          <input
+            value={formState.name}
+            onChange={(e) =>
+              setFormState({ ...formState, name: e.target.value })
+            }
+            type='text'
+            placeholder='Your name'
+          />
+        )}
+        <input
+          value={formState.email}
+          onChange={(e) =>
+            setFormState({ ...formState, email: e.target.value })
+          }
+          type='text'
+          placeholder='Your email address'
+        />
+        <input
+          value={formState.password}
+          onChange={(e) =>
+            setFormState({ ...formState, password: e.target.value })
+          }
+          type='text'
+          placeholder='Your password'
+        />
+      </div>
+    </div>
+  )
 }
