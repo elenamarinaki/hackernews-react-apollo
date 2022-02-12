6e4ac3ee30d0cddef8a4ec20296ae877179b362e
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-02-12 00:45:28 +0000

    updating the header with login option

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { CreateLink } from "./CreateLink"
 // import { Signup } from "./Signup"
 import { Header } from "./Header"
 import { Route, Routes } from "react-router-dom"
+import { Login } from "./Login"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<LinkList />} />
           <Route path='/create' element={<CreateLink />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </div>
     </div>
