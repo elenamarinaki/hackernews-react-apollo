8c2baea3f9af959b039a500ffdee02fea1833b22
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-02-15 23:57:33 +0000

    login mutation

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,6 +9,19 @@ const SIGNUP_MUTATION = gql`
     }
   }
 `
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation(
+    $email: String!
+    $password: String!
+  ) {
+    login(
+      email: $email,
+      password: $$password
+    ) {
+      token
+    }
+  }
+`
 
 export const Login = () => {
   const navigate = useNavigate()
